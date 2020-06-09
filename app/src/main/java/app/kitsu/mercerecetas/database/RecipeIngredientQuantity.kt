608c91df5eba280230388897b04fe3ab56c1d66c
kitsu <kitsu@mail.com>
2020-06-09 23:46:08 +0200

    add FK constraint to embedded Ingredient

--- app/src/main/java/app/kitsu/mercerecetas/database/RecipeIngredientQuantity.kt
+++ app/src/main/java/app/kitsu/mercerecetas/database/RecipeIngredientQuantity.kt
@@ -4,7 +4,9 @@ import androidx.room.*
 
 
 @Entity(tableName = "recipe_ingredient_quantity_table",
-        foreignKeys = arrayOf(ForeignKey(entity = Recipe::class, parentColumns = ["recipeId"], childColumns = ["recipe_id"])))
+        foreignKeys = arrayOf(ForeignKey(entity = Recipe::class, parentColumns = ["recipeId"], childColumns = ["recipe_id"]),
+            //delete this to avoid issues with constraints if needed for testing purposes
+            ForeignKey(entity = Ingredient::class, parentColumns = ["i_id"], childColumns = ["ing_i_id"])))
 data class RecipeIngredientQuantity (
 
     @PrimaryKey(autoGenerate = true)
